5b887acc2ff1eaf9563de682880a3b53d442430a
giosermon <2172@holbertonschool.com>
2021-08-15 22:16:31 -0500

    update advance

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,13 @@
 # connect to a server Execute a command
 
-file_line{'/etc/ssh/ssh_config'
-  ensure  => 'present',
-  content => 'PasswordAuthentication no
-              IdentityFile ~/.ssh/holberton'
+file_line { 'assword no auth':
+    ensure => 'present',
+    path   => '/etc/ssh/ssh_config',
+    line   => 'PasswordAuthentication no',
+}
+
+file_line { 'file_identity':
+    ensure => 'present',
+    path   => '/etc/ssh/ssh_config',
+    line   => 'IdentityFile ~/.ssh/holberton',
 }
